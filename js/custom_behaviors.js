1fa94122a709e445a2c89e9d3e0ae36649cbab61
Noahdelagardelle <noahcdela@gmail.com>
2017-02-13 23:33:40 -0600

    animation

--- js/custom_behaviors.js
+++ js/custom_behaviors.js
@@ -1,22 +1,55 @@
 $(document).ready(function() {
-/*
-	$('.about-me-btn').on('tap', function(){
-		$('.about-me-modal').addClass('modal-open');
-	})
 
-	$('.close-modal-btn').on('tap', function(){
-		$('.about-me-modal').removeClass('modal-open');
-	})
-*/
+	animate()
 
 	$('.about-me-btn').on('tap', function(e){
 		e.preventDefault()
 		$('.about-me-modal').toggleClass('modal-open');
 		$(this).toggleClass('close-btn')
+		modalAnimate()
 	})
 
-	//$('.close-modal-btn').on('tap', function(){
-	//	$('.about-me-modal').removeClass('modal-open');
-	//})
+	function animate() {
+		$('.intro').addClass('animated fadeInUp');
+		setTimeout(function(){
+		  $('.body').addClass('animated fadeInUp');
+		}, 600);
+		setTimeout(function(){
+			$('.email-label, .input-container').addClass('animated fadeInUp');
+		}, 900);
+		setTimeout(function(){
+			$('.social-container a:nth-child(1)').addClass('animated fadeInUp');
+		}, 1200);
+		setTimeout(function(){
+			$('.social-container a:nth-child(2)').addClass('animated fadeInUp');
+		}, 1250);
+		setTimeout(function(){
+			$('.social-container a:nth-child(3)').addClass('animated fadeInUp');
+		}, 1300);
+		setTimeout(function(){
+			$('.social-container a:nth-child(4)').addClass('animated fadeInUp');
+		}, 1350);
+		setTimeout(function(){
+			$('.social-container a:nth-child(5)').addClass('animated fadeInUp');
+		}, 1400);
+		setTimeout(function(){
+			$('.social-container a:nth-child(6)').addClass('animated fadeInUp');
+		}, 1450);
+	};
+
+	function modalAnimate() {
+		setTimeout(function(){
+			$('.aside-container h2').toggleClass('animated fadeInRight');
+		}, 100);
+		setTimeout(function(){
+			$('.aside-container p:nth-child(2)').toggleClass('animated fadeInRight');
+		}, 150);
+		setTimeout(function(){
+			$('.aside-container p:nth-child(3)').toggleClass('animated fadeInRight');
+		}, 200);
+		setTimeout(function(){
+			$('.aside-container p:nth-child(4)').toggleClass('animated fadeInRight');
+		}, 210);
+	};
 
 });
