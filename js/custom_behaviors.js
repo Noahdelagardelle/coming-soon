d08741585f3687c231d1e006df96537c383b96ac
Noahdelagardelle <noahcdela@gmail.com>
2017-02-13 20:34:48 -0600

    init

---
+++ js/custom_behaviors.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+	$('.about-me-btn').on('tap', function(){
+		$('.about-me-modal').addClass('modal-open');
+	})
+
+	$('.close-modal-btn').on('tap', function(){
+		$('.about-me-modal').removeClass('modal-open');
+	})
+
+	// VARIABLES
+
+
+	// EVENT HANDLERS
+
+
+	// FUNCTIONS
+
+
+	// INITIALIZATION
+
+
+});
---
+++ js/min/custom_behaviors-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".about-me-btn").on("tap",function(){$(".about-me-modal").addClass("modal-open")}),$(".close-modal-btn").on("tap",function(){$(".about-me-modal").removeClass("modal-open")})});
